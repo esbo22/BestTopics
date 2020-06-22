4e11841b8e7ac9c2e337821d9aaaf30f2ea3d311
Johnny Girault <esbo@hotmail.fr>
2020-06-22 21:39:48 +0200

    test annotate

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -15,3 +15,19 @@ end
 # topic.title
 # topic.content
 # topic.???
+
+# ## Schema Information
+#
+# Table name: `topics`
+#
+# ### Columns
+#
+# Name              | Type               | Attributes
+# ----------------- | ------------------ | ---------------------------
+# **`id`**          | `integer`          | `not null, primary key`
+# **`content`**     | `text`             |
+# **`published`**   | `boolean`          | `default(TRUE)`
+# **`title`**       | `string`           |
+# **`created_at`**  | `datetime`         | `not null`
+# **`updated_at`**  | `datetime`         | `not null`
+#
--- test/models/topic_answer_test.rb
+++ test/models/topic_answer_test.rb
@@ -5,3 +5,19 @@ class TopicAnswerTest < ActiveSupport::TestCase
   #   assert true
   # end
 end
+
+# ## Schema Information
+#
+# Table name: `topic_answers`
+#
+# ### Columns
+#
+# Name              | Type               | Attributes
+# ----------------- | ------------------ | ---------------------------
+# **`id`**          | `integer`          | `not null, primary key`
+# **`content`**     | `text`             |
+# **`published`**   | `boolean`          |
+# **`title`**       | `string`           |
+# **`created_at`**  | `datetime`         | `not null`
+# **`updated_at`**  | `datetime`         | `not null`
+#
